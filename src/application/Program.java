19cc9e0b9ffbfb9c9663f32e692f693417ea2840
JoaoAslan <contato.joaoaslan@gmail.com>
2022-10-05 17:05:10 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,12 @@
 package application;
 
+import boardlayer.Board;
 import boardlayer.Position;
 
 public class Program {
     public static void main(String[] args) {
 
-        // TESTANDO A CLASSE POSITION
-        Position position = new Position(5, 3);
-        System.out.println(position);
-
+        Board board = new Board(8,8);
+        
     }
 }
